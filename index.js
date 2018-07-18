3d89039779cc1ddeaf7f9dfb9c8bafe752699c71
morgsmccauley <morgan.mccauley@vodafone.com>
2018-07-19 07:31:32 +1200

    Refactor, Add file / folder deletion

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const fs = require('fs');
 const path = require('path');
+const rimraf = require('rimraf');
 const R = require('ramda');
+
+const filterP = (funcP, arr) => (
+  R.pipeP(
+    ...arr.map(el => output => funcP(el).then(
+      outputBool => Promise.resolve(
+        outputBool ? R.append(el, output) : output,
+      ),
+    )),
+  )([])
+);
 
 const isDirectory = dir => new Promise((resolve, reject) => {
   fs.stat(dir, (err, stat) => {
@@ -10,40 +21,65 @@ const isDirectory = dir => new Promise((resolve, reject) => {
   });
 });
 
-const getChildrenFromDirectory = parentDir => new Promise((resolve, reject) => {
-  const getAbsPath = relPath => path.join(parentDir, relPath);
+const getSubDirectories = children => filterP(
+  isDirectory,
+  children,
+);
 
-  fs.readdir(parentDir, async (err, children) => {
+const getChildrenFromDirectory = directory => new Promise((resolve, reject) => {
+  fs.readdir(directory, (err, children) => {
     if (err) reject(err);
-    if (!children.length) resolve([]);
+    if (!children.length) reject();
 
-    const fileList = [];
-    const dirList = [];
-
-    const childIsDirectoryMap = await Promise.all(
-      R.pipe(
-        R.map(getAbsPath),
-        R.map(isDirectory),
-      )(children),
-    );
-
-    children.forEach((child, index) => {
-      if (childIsDirectoryMap[index]) {
-        dirList.push(child);
-      } else {
-        fileList.push(child);
-      }
-    });
-
-    const filesFromChildDirectories = await Promise.all(
-      R.pipe(
-        R.map(getAbsPath),
-        R.map(getChildrenFromDirectory),
-      )(dirList),
-    );
-
-    resolve(R.flatten(fileList.concat(filesFromChildDirectories)));
+    resolve(children);
   });
 });
 
-getChildrenFromDirectory('../myvf-reactnative/App').then(data => console.log(data));
+// TODO:
+//  - handle rejection
+//  - add included file path
+//  - integrate with main branch
+
+const getFilesRecursive = rootDir => new Promise(async (resolve, reject) => {
+  const getAbsPath = relPath => path.join(rootDir, relPath);
+
+  const childrenRelative = await getChildrenFromDirectory(rootDir);
+  const children = childrenRelative.map(child => getAbsPath(child));
+  const subDirectories = await getSubDirectories(children);
+  const files = R.uniq(children, subDirectories); // Not the right ramda function
+
+  const filesFromSubDirectories = await Promise.all(
+    R.map(getFilesRecursive, subDirectories),
+  );
+
+  resolve(
+    R.flatten(
+      files.concat(filesFromSubDirectories),
+    ),
+  );
+});
+
+const removeFileOrDirectory = inputPath => new Promise((resolve, reject) => {
+  const rejectIfErr = (err) => {
+    if (err) reject(err);
+  };
+  const removeFile = filePath => fs.unlink(filePath, rejectIfErr);
+  const removeDir = dirPath => rimraf(dirPath, rejectIfErr);
+
+  fs.unlink(inputPath, async (err) => {
+    rejectIfErr(err);
+
+    R.ifElse(
+      await isDirectory,
+      removeDir,
+      removeFile,
+    )(inputPath);
+
+    resolve();
+  });
+});
+
+module.exports = {
+  getFilesRecursive,
+  removeFileOrDirectory,
+};
